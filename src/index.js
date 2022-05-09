a12e5eef10319d9bd10cdf5e21e89ec5dcf39be5
arty63 <artjom63@mail.ru>
2022-05-10 01:39:49 +0400

    fix: pushing on the buttons

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 //import './styles/styles.css'
 import "./styles/main.scss" //импортим стили из scss
 
-import {firstRow, firstRowShift,firstRowRu, firstRowShiftRu,
-     secRow, secRowShift, secRowRu, secRowShiftRu,
-      thirdRow, thirdRowShift, thirdRowRu, thirdRowShiftRu,
-       fourRow, fourRowShift,  fourRowRu, fourRowShiftRu,
-        fivRow, fivRowShift, fivRowRu, fivRowShiftRu
+import {firstRow, firstRowShift,firstRowRu, firstRowShiftRu, firstRowCode,
+     secRow, secRowShift, secRowRu, secRowShiftRu, secRowCode,
+      thirdRow, thirdRowShift, thirdRowRu, thirdRowShiftRu, thirdRowCode,
+       fourRow, fourRowShift,  fourRowRu, fourRowShiftRu, fourRowCode,
+        fivRow, fivRowShift, fivRowRu, fivRowShiftRu,fivRowCode,
     } from './symbols.js'  //импортим массивы с буквами для создания кнопок
 
 const WRAPPER = document.createElement('div')
@@ -43,61 +43,96 @@ LANG.classList = 'lang'
 WRAPPER.appendChild(LANG)
 LANG.textContent = 'Для переключения языка комбинация: левыe ctrl + alt'
 
-const row = (symbols, symbolsShift) =>{
+const row = (symbols, symbolsShift, symbolsRu, symbolsShiftRu, symbolsCode) =>{
     const newRow = document.createElement('div')
     newRow.classList = 'row'
-    BOARD.appendChild(newRow)
+    BOARD.appendChild(newRow)                 //создали строку
     for (let i=0; i<symbols.length; i++){
-        const btn = document.createElement('div')
+        const btn = document.createElement('div')  //создаем контейнер для кнопки
         btn.classList = 'btn'
-        btn.classList.add(`${symbols[i]}`)
+        btn.classList.add(`${symbolsCode[i]}`)
         newRow.appendChild(btn)
-        const btnEn = document.createElement('span')
+
+        const btnEn = document.createElement('span') //создаем содержимое EN кнопки
         btnEn.classList = 'btn-en'
         btn.appendChild(btnEn)
         btnEn.textContent = `${symbols[i]}`
-        const btnEnShift = document.createElement('span')
+
+        const btnEnShift = document.createElement('span') //создаем содержимое EN-shift кнопки
         btnEnShift.classList = 'btn-en-shift'
         btnEnShift.classList.add('unvisible')
         btn.appendChild(btnEnShift)
         btnEnShift.textContent = `${symbolsShift[i]}`
+
+        const btnRu = document.createElement('span') //создаем содержимое RU кнопки
+        btnRu.classList = 'btn-ru'
+        btnRu.classList.add('unvisible')
+        btn.appendChild(btnRu)
+        btnRu.textContent = `${symbolsRu[i]}`
+
+        const btnRuShift = document.createElement('span') //создаем содержимое RU кнопки
+        btnRuShift.classList = 'btn-ru-shift'
+        btnRuShift.classList.add('unvisible')
+        btn.appendChild(btnRuShift)
+        btnRuShift.textContent = `${symbolsShiftRu[i]}`
+
+        
         document.addEventListener('keydown', function(event) {
             if (event.code == 'CapsLock') {
                 btnEnShift.classList.toggle('unvisible')
                 btnEn.classList.toggle('unvisible')
             }
           });
-
+          document.onkeydown = function(event){
+            if(event.code == "ControlLeft"){
+                document.onkeyup = function(event){
+                    if(event.code == "AltLeft"){ 
+                        btnRu.classList.toggle('unvisible')
+                        btnEn.classList.toggle('unvisible')
+                    }
+                }    
+            }
+        }  
     } 
 }
-row(firstRow, firstRowShift)
-row(secRow, secRowShift)
-row(thirdRow, thirdRowShift)
-row(fourRow,fourRowShift)
-row(fivRow, fivRowShift)
+row(firstRow, firstRowShift, firstRowRu, firstRowShiftRu, firstRowCode)
+row(secRow, secRowShift, secRowRu, secRowShiftRu, secRowCode)
+row(thirdRow, thirdRowShift, thirdRowRu, thirdRowShiftRu,thirdRowCode)
+row(fourRow,fourRowShift, fourRowRu,fourRowShiftRu, fourRowCode)
+row(fivRow, fivRowShift, fivRowRu, fivRowShiftRu,fivRowCode)
 
-document.addEventListener('keydown', function(event) {
-            if (event.code == 'CapsLock') {  
-                document.querySelector('.CapsLock').classList.toggle('active')
-            }
-          });
 
-const pushBtn = (event) =>{
-    //console.log(event.target.closest())   нужно доделать
-    if(event.target.textContent.length < 3){
-        AREA.textContent+=event.target.textContent
+document.onkeydown = function(event){
+    document.querySelector(`.${event.code}`).classList.add('active')
+    document.onkeyup = function(event){
+        document.querySelector(`.${event.code}`).classList.remove('active')
     }
 }
-const btn = document.querySelectorAll('.btn')
-btn.forEach((element) => element.addEventListener('click', pushBtn));        
 
-document.addEventListener('keydown', function(event) {
-    console.log(event.key)
-    document.querySelector(`.${event.key}`).classList.add('active')
-    if(event.key.length < 3){
-        AREA.textContent+=event.key
-    }
-})
-document.addEventListener('keyup', function(event) {
-    document.querySelector(`.${event.key}`).classList.remove('active')
-})
+// document.addEventListener('keydown', function(event) {
+//             if (event.code == 'CapsLock') {  
+//                 document.querySelector('.CapsLock').classList.toggle('active')
+//             }
+//           });
+
+// const pushBtn = (event) =>{
+//     //console.log(event.target.closest())   нужно доделать
+//     if(event.target.textContent.length < 3){
+//         AREA.textContent+=event.target.textContent
+//     }
+// }
+// const btn = document.querySelectorAll('.btn')
+// btn.forEach((element) => element.addEventListener('click', pushBtn));        
+
+// document.addEventListener('keydown', function(event) {
+//     console.log(event)
+//     document.querySelector(`.${event.code}`).classList.add('active')
+//     if(event.key.length < 5){
+//         AREA.textContent+=event.key
+//     }
+// })
+// document.addEventListener('keyup', function(event) {
+//     if(event.code != 'CapsLock' ){
+//         document.querySelector(`.${event.key}`).classList.remove('active')
+//     }
+// })
--- src/symbols.js
+++ src/symbols.js
@@ -2,29 +2,30 @@ export const firstRow = ['`','1','2','3','4','5','6','7','8','9','0','-','=', 'B
 export const firstRowShift = ['~','!','@','#', '$','%', '^', '&','*','(',')', '_','+','Backspace']
 export const firstRowRu = ['ё','1','2','3','4','5','6','7','8','9','0','-','=', 'Backspace']
 export const firstRowShiftRu = ['Ё','!','"','№', ';','%', ':', '?','*','(',')', '_','+','Backspace']
-
+export const firstRowCode =['Backquote','Digit1','Digit2','Digit3','Digit4','Digit5','Digit6','Digit7','Digit8','Digit9','Digit0','Minus','Equal', 'Backspace']
 
 
 export const secRow = ['Tab','q','w','e','r','t','y','u','i','o','p','[',']','\\','Del']
 export const secRowShift = ['Tab','Q','W','E','R','T','Y','U','I','O','P','{','}','|','Del']  
 export const secRowRu = ['Tab','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','\\','Del']
 export const secRowShiftRu = ['Tab','Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','/','Del']  
-
+export const secRowCode = ['Tab','KeyQ','KeyW','KeyE','KeyR','KeyT','KeyY','KeyU','KeyI','KeyO','KeyP','BracketLeft','BracketRight','Backslash','Delete']
 
 
 export const thirdRow =['CapsLock', 'a','s','d','f','g','h','j','k','l',';',"'",'Enter']
 export const thirdRowShift =['CapsLock', 'A','S','D','F','G','H','J','K','L',':','"','Enter']
 export const thirdRowRu =['CapsLock', 'ф','ы','в','а','п','р','о','л','д','ж',"э",'Enter']
 export const thirdRowShiftRu =['CapsLock', 'Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter']
-
+export const thirdRowCode = ['CapsLock', 'KeyA','KeyS','KeyD','KeyF','KeyG','KeyH','KeyJ','KeyK','KeyL','Semicolon','Quote','Enter']
 
 export const fourRow = ['Shift','z', 'x', 'c','v','b','n','m',',','.','\/','▲','Shift']
 export const fourRowShift = ['Shift','Z', 'X', 'C','V','B','N','M','<','>','?','▲','Shift']
 export const fourRowRu = ['Shift','я', 'ч', 'с','м','и','т','ь','б','ю','.','▲','Shift']
 export const fourRowShiftRu = ['Shift','Я', 'Ч', 'С','М','И','Т','Ь','Б','Ю',',','▲','Shift']
-
+export const fourRowCode =['ShiftLeft', 'KeyZ','KeyX', 'KeyC','KeyV','KeyB','KeyN','KeyM','Comma','Period','Slash','ArrowUp','ShiftRight']
 
 export const fivRow = ['Ctrl','WIN', 'Alt','Space','Alt','Ctrl','◄','▼','►',]
 export const fivRowShift = ['Ctrl','WIN', 'Alt','Space','Alt','Ctrl','◄','▼','►',]
 export const fivRowRu = ['Ctrl','WIN', 'Alt','Space','Alt','Ctrl','◄','▼','►',]
 export const fivRowShiftRu = ['Ctrl','WIN', 'Alt','Space','Alt','Ctrl','◄','▼','►',]
+export const fivRowCode = ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight','ArrowLeft', 'ArrowDown','ArrowRight']
