bd021e414758a27ea9c8c85c462121cb4f7476e9
arty63 <artjom63@mail.ru>
2022-05-09 19:28:41 +0400

    feat: add pushing on the buttons

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 //import './styles/styles.css'
-import "./styles/main.scss"
+import "./styles/main.scss" //импортим стили из scss
 
-import {firstRow, firstRowShift, secRow, secRowShift, thirdRow, thirdRowShift, fourRow, fourRowShift, fivRow, fivRowShift} from './symbols.js'
-
+import {firstRow, firstRowShift,firstRowRu, firstRowShiftRu,
+     secRow, secRowShift, secRowRu, secRowShiftRu,
+      thirdRow, thirdRowShift, thirdRowRu, thirdRowShiftRu,
+       fourRow, fourRowShift,  fourRowRu, fourRowShiftRu,
+        fivRow, fivRowShift, fivRowRu, fivRowShiftRu
+    } from './symbols.js'  //импортим массивы с буквами для создания кнопок
 
 const WRAPPER = document.createElement('div')
 WRAPPER.classList = 'wrapper'
-document.body.appendChild(WRAPPER)
+document.body.appendChild(WRAPPER)//создаем див враппер и кладем в документ
 
-const TITLE = document.createElement('h1')
+const TITLE = document.createElement('h1')//создаем заголовок
 TITLE.classList = 'title'
 WRAPPER.appendChild(TITLE)
 TITLE.textContent = 'RSS Виртуальная клавиатура'
 
-const COVER = document.createElement('div')
+const COVER = document.createElement('div')//создаем псевдомонитор
 COVER.classList = 'frame'
 WRAPPER.appendChild(COVER)
 
 const MAIN_SCREEN = document.createElement('div')
 MAIN_SCREEN.classList = 'main-screen'
-COVER.appendChild(MAIN_SCREEN)
+COVER.appendChild(MAIN_SCREEN)//создаем псевдомонитор
+
+const AREA = document.createElement('textarea')
+AREA.classList = 'area'
+COVER.appendChild(AREA)//создаем локацию для ввода с клавиатуры и нажатия мышкой
 
 const BOARD = document.createElement('div')
 BOARD.classList = 'board'
@@ -42,6 +50,7 @@ const row = (symbols, symbolsShift) =>{
     for (let i=0; i<symbols.length; i++){
         const btn = document.createElement('div')
         btn.classList = 'btn'
+        btn.classList.add(`${symbols[i]}`)
         newRow.appendChild(btn)
         const btnEn = document.createElement('span')
         btnEn.classList = 'btn-en'
@@ -53,12 +62,12 @@ const row = (symbols, symbolsShift) =>{
         btn.appendChild(btnEnShift)
         btnEnShift.textContent = `${symbolsShift[i]}`
         document.addEventListener('keydown', function(event) {
-            if (event.code == 'ShiftLeft') {
+            if (event.code == 'CapsLock') {
                 btnEnShift.classList.toggle('unvisible')
                 btnEn.classList.toggle('unvisible')
-               
             }
           });
+
     } 
 }
 row(firstRow, firstRowShift)
@@ -67,4 +76,28 @@ row(thirdRow, thirdRowShift)
 row(fourRow,fourRowShift)
 row(fivRow, fivRowShift)
 
+document.addEventListener('keydown', function(event) {
+            if (event.code == 'CapsLock') {  
+                document.querySelector('.CapsLock').classList.toggle('active')
+            }
+          });
 
+const pushBtn = (event) =>{
+    //console.log(event.target.closest())   нужно доделать
+    if(event.target.textContent.length < 3){
+        AREA.textContent+=event.target.textContent
+    }
+}
+const btn = document.querySelectorAll('.btn')
+btn.forEach((element) => element.addEventListener('click', pushBtn));        
+
+document.addEventListener('keydown', function(event) {
+    console.log(event.key)
+    document.querySelector(`.${event.key}`).classList.add('active')
+    if(event.key.length < 3){
+        AREA.textContent+=event.key
+    }
+})
+document.addEventListener('keyup', function(event) {
+    document.querySelector(`.${event.key}`).classList.remove('active')
+})
--- src/symbols.js
+++ src/symbols.js
@@ -1,22 +1,30 @@
 export const firstRow = ['`','1','2','3','4','5','6','7','8','9','0','-','=', 'Backspace']
 export const firstRowShift = ['~','!','@','#', '$','%', '^', '&','*','(',')', '_','+','Backspace']
+export const firstRowRu = ['ё','1','2','3','4','5','6','7','8','9','0','-','=', 'Backspace']
+export const firstRowShiftRu = ['Ё','!','"','№', ';','%', ':', '?','*','(',')', '_','+','Backspace']
 
 
 
 export const secRow = ['Tab','q','w','e','r','t','y','u','i','o','p','[',']','\\','Del']
 export const secRowShift = ['Tab','Q','W','E','R','T','Y','U','I','O','P','{','}','|','Del']  
+export const secRowRu = ['Tab','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','\\','Del']
+export const secRowShiftRu = ['Tab','Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','/','Del']  
 
 
 
 export const thirdRow =['CapsLock', 'a','s','d','f','g','h','j','k','l',';',"'",'Enter']
 export const thirdRowShift =['CapsLock', 'A','S','D','F','G','H','J','K','L',':','"','Enter']
+export const thirdRowRu =['CapsLock', 'ф','ы','в','а','п','р','о','л','д','ж',"э",'Enter']
+export const thirdRowShiftRu =['CapsLock', 'Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter']
 
 
 export const fourRow = ['Shift','z', 'x', 'c','v','b','n','m',',','.','\/','▲','Shift']
 export const fourRowShift = ['Shift','Z', 'X', 'C','V','B','N','M','<','>','?','▲','Shift']
-
-
+export const fourRowRu = ['Shift','я', 'ч', 'с','м','и','т','ь','б','ю','.','▲','Shift']
+export const fourRowShiftRu = ['Shift','Я', 'Ч', 'С','М','И','Т','Ь','Б','Ю',',','▲','Shift']
 
 
 export const fivRow = ['Ctrl','WIN', 'Alt','Space','Alt','Ctrl','◄','▼','►',]
 export const fivRowShift = ['Ctrl','WIN', 'Alt','Space','Alt','Ctrl','◄','▼','►',]
+export const fivRowRu = ['Ctrl','WIN', 'Alt','Space','Alt','Ctrl','◄','▼','►',]
+export const fivRowShiftRu = ['Ctrl','WIN', 'Alt','Space','Alt','Ctrl','◄','▼','►',]
